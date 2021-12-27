d062c61577d0fbe045bd44d90f5d0b257fbca656
ckk <1499381220@qq.com>
2021-12-27 18:22:43 +0800

    设置单位

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <!-- 全局主题 -->
+  <van-config-provider :theme-vars="themeVars">
+    <!-- 开启顶部安全区适配 -->
+    <van-nav-bar safe-area-inset-top />
+
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+
+    <!-- 开启底部安全区适配 -->
+    <van-number-keyboard safe-area-inset-bottom />
+  </van-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    // themeVars 内的值会被转换成对应 CSS 变量
+    // 比如 sliderBarHeight 会转换成 `--van-slider-bar-height`
+    const themeVars = {
+    }
+
+    return {
+      themeVars
+    };
+  },
+})
+</script>
+
 
 <style lang="less">
 #app {
